c70c7f1c44a7175b1205c2e643a029600f10daa6
Arthur Korchagin <lliepmah@mail.ru>
2016-07-05 10:12:58 +0300

    Test signing and lambdas

--- app/src/main/java/ru/kodep/sample/sampleproject/MainActivity.java
+++ app/src/main/java/ru/kodep/sample/sampleproject/MainActivity.java
@@ -1,13 +1,20 @@
 package ru.kodep.sample.sampleproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.ac_main);
+        findViewById(R.id.ac_main_tv_test).setOnClickListener(this::testLambda);
+    }
+
+    private void testLambda(View view) {
+        Log.d(MainActivity.class.getName(), "-> testLambda ->");
     }
 }
