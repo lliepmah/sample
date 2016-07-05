05cb9c30c41390b2c3fa583eb5f63bd74daf39b2
Arthur Korchagin <lliepmah@mail.ru>
2016-07-05 15:52:39 +0300

    Added debug configuration

--- presentation/src/test/java/ru/kodep/sample/sampleproject/ExampleUnitTest.java
+++ presentation/src/test/java/ru/kodep/sample/sampleproject/ExampleUnitTest.java
@@ -2,14 +2,19 @@ package ru.kodep.sample.sampleproject;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void addition_isAnother() {
+        assertEquals(6, 3 * 2);
     }
 }
